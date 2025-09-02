dad2f6fa21c2110aa67e746314734dd86e180567
Sonu Das <sonu757om@gmail.com>
2025-09-02 12:01:57 +0000

    What People Are Saying add a oreng colors

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -38,7 +38,7 @@ export default function Testimonials() {
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline text-primary">
               What People Are Saying
             </h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
