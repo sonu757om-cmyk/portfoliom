170e5ab51cc221f1aecd6e43d412a498c6a5ff5b
Sonu Das <sonu757om@gmail.com>
2025-09-02 13:10:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
             </div>
           </div>
           <Image
-            src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw0fHxtYW58ZW58MHx8fHwxNzU2ODExMjM2fDA&ixlib=rb-4.1.0&q=80&w=1080"
+            src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxOXx8cGVyc29uJTIwfGVufDB8fHx8MTc1NjgxODYwNXww&ixlib=rb-4.1.0&q=80&w=1080"
             width={600}
             height={600}
             alt="Hero"
